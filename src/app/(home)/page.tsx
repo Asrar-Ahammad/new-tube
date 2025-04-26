8b7aeaeaafa5cbc4e043e8b5edd5f9685381f9c0
Asrar Ahammad <asrarahammadshaik@gmail.com>
2025-04-26 08:27:39 +0530

    Completed tRPC steup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 
-export default function Home() {
- return(
-  <div> 
-    I willl load videos in the future!
-  </div>
- )  
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Asrar" });
+
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
